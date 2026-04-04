622772bfdc84ee3adde6acb2e060575ee75a6131
Jissena <jissmailbis@gmail.com>
2026-04-04 16:11:56 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,11 @@
-export const metadata = {
-  title: 'GitHub Proxy',
-  description: 'Protected GitHub Raw Proxy',
-}
+export const metadata = { title: 'JS Script - Protected' }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
-      <body>{children}</body>
+      <body style={{margin:0,padding:0,overflow:'hidden'}}>
+        {children}
+      </body>
     </html>
   )
 }
